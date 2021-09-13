8fd9645bf34c1bd28a31429c4d4f08e237b754a1
alok <alok.sharma.17@mountblue.tech>
2021-09-13 16:57:55 +0530

    use ContextApi

---
+++ src/Components/List.js
@@ -0,0 +1,19 @@
+import React,{ useContext } from 'react';
+import { listContext } from './App';
+import Itemlist from './Itemslist';
+const List = () => {
+  const { items } = useContext(listContext)
+  return (
+    <div>
+      {items.map((item) => {
+        return (
+          <Itemlist item={item} key={item.id}/>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
+
+
