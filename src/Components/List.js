498a24466a5635d03e5eeff1b01bc410707618f0
alok <alok.sharma.17@mountblue.tech>
2021-09-13 17:08:26 +0530

    Apply eslint

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,19 +1,15 @@
-import React,{ useContext } from 'react';
-import { listContext } from './App';
-import Itemlist from './Itemslist';
+import React, { useContext } from "react";
+import { listContext } from "./App";
+import Itemlist from "./Itemslist";
 const List = () => {
-  const { items } = useContext(listContext)
+  const { items } = useContext(listContext);
   return (
     <div>
       {items.map((item) => {
-        return (
-          <Itemlist item={item} key={item.id}/>
-        );
+        return <Itemlist item={item} key={item.id} />;
       })}
     </div>
   );
 };
 
 export default List;
-
-
